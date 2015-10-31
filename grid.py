2c1c7570c60d4f4844fdf6436269f824e21cca84
mdhk <mdhk@users.noreply.github.com>
2015-10-31 12:54:55 +0100

    added function to set gates
    set_gates sets gates from print info file to grid

--- grid.py
+++ grid.py
@@ -1,8 +1,16 @@
-width = 18
-height = 13
+from print1 import width, height, gates
+
 depth = 1
 
-grid = [[[0 for z in range(depth)]for y in range(height)] for x in range(width)]
+grid = [[[0 for z in range(depth)] for x in range(width)] for y in range(height)]
+
+def set_gates( gates ):
+    for gate in range(len(gates)):
+        gate_x = gates[gate][1]
+        gate_y = gates[gate][2]
+        grid[gate_y][gate_x] = gates[gate][0]
+
+set_gates ( gates )
 
 for x in range(len(grid)):
     print grid[x]
