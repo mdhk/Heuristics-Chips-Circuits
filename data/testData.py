82aded236af700e5912df1d6bedaf20bc1ea0ee3
Maurits <maurits.ridder@gmail.com>
2015-11-02 15:45:40 +0100

    added test-data

---
+++ data/testData.py
@@ -0,0 +1,15 @@
+# testData
+# Heuristics, Chips & Circuits, Fall 2015
+# HaMaMa
+
+# grid width and height
+width = 5
+height = 5 
+
+# gate coordinates, template: [gate_number, gate_x, gate_y]
+gates = [
+        [1, 3],
+        [1, 1],
+        [2, 4],
+        [3, 2],
+        ]
