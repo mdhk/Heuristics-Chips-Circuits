32ffa24576b4f5965fcd0191f75f8e7628dbc1ed
mdhk <mdhk@users.noreply.github.com>
2015-11-02 13:54:08 +0100

    gate 25 toegevoegd

--- grid.py
+++ grid.py
@@ -4,14 +4,14 @@ depth = 1
 
 grid = [[[0 for z in range(depth)] for x in range(width)] for y in range(height)]
 
-def set_gates( gates ):
+def set_gates(gates):
     for gate in range(len(gates)):
         print gate
         gate_x = gates[gate][1]
         gate_y = gates[gate][2]
         grid[gate_y][gate_x] = gates[gate][0]
 
-set_gates ( gates )
+set_gates(gates)
 
 for x in range(len(grid)):
     print grid[x]
