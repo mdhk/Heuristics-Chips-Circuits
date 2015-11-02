059c5ca9e2b14b12eda92f1ff5ce916eeeed477e
Hasine Efeturk <hasineefeturk@hotmail.com>
2015-11-02 14:12:53 +0100

    setgates veranderd

--- grid.py
+++ grid.py
@@ -1,15 +1,12 @@
-from print1 import width, height, gates
+from config1 import width, height, gates
 
 depth = 1
 
 grid = [[[0 for z in range(depth)] for x in range(width)] for y in range(height)]
 
 def set_gates(gates):
-    for gate in range(len(gates)):
-        print gate
-        gate_x = gates[gate][1]
-        gate_y = gates[gate][2]
-        grid[gate_y][gate_x] = gates[gate][0]
+    for gate in gates:
+         grid[gate[1]][gate[0]] = 1
 
 set_gates(gates)
 
---
+++ print2.py
@@ -0,0 +1,18 @@
+"""
+Info for print 1
+Heuristics, Chips & Circuits, Fall 2015
+HaMaMa
+"""
+
+# Grid width and height (starting from zero).  
+height = 16 
+width = 17
+
+# Gate coordinates, template: gate_x, gate_y
+import csv
+gates = []
+with open('Print2.csv', 'rb') as csvfile:
+    # Without QUOTE_NONNUMERIC the numbers become strings
+    csvfile = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+    for row in csvfile:
+        gates.append(row)
