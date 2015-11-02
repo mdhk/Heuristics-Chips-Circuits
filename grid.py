6b2c6252e3bced1275cc65f416f7b4dd4372effe
mdhk <mdhk@users.noreply.github.com>
2015-11-02 13:56:01 +0100

    grid.py importeert nu config1 ipv print1

--- grid.py
+++ grid.py
@@ -1,4 +1,4 @@
-from print1 import width, height, gates
+from config1 import width, height, gates
 
 depth = 1
 
@@ -6,7 +6,6 @@ grid = [[[0 for z in range(depth)] for x in range(width)] for y in range(height)
 
 def set_gates(gates):
     for gate in range(len(gates)):
-        print gate
         gate_x = gates[gate][1]
         gate_y = gates[gate][2]
         grid[gate_y][gate_x] = gates[gate][0]
@@ -15,3 +14,5 @@ set_gates(gates)
 
 for x in range(len(grid)):
     print grid[x]
+
+class Gate
