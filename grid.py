a441e76c2b417205d05bcef9b200e1ec90f83078
mdhk <mdhk@users.noreply.github.com>
2015-11-02 14:01:51 +0100

    aangepast voor nieuwe configlijst

--- grid.py
+++ grid.py
@@ -1,4 +1,4 @@
-from config1 import width, height, gates
+from config2 import width, height, gates
 
 depth = 1
 
@@ -6,13 +6,13 @@ grid = [[[0 for z in range(depth)] for x in range(width)] for y in range(height)
 
 def set_gates(gates):
     for gate in range(len(gates)):
-        gate_x = gates[gate][1]
-        gate_y = gates[gate][2]
-        grid[gate_y][gate_x] = gates[gate][0]
+        gate_x = gates[gate][0]
+        gate_y = gates[gate][1]
+        grid[gate_y][gate_x] = gates.index(gate) + 1
 
 set_gates(gates)
 
 for x in range(len(grid)):
     print grid[x]
 
-class Gate
+# class Gate
