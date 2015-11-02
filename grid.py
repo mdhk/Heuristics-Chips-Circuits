6eee5410e553d428003e294e4d471d5d4afedaaa
Maurits <maurits.ridder@gmail.com>
2015-11-02 13:52:39 +0100

    filename changed

--- grid.py
+++ grid.py
@@ -6,6 +6,7 @@ grid = [[[0 for z in range(depth)] for x in range(width)] for y in range(height)
 
 def set_gates( gates ):
     for gate in range(len(gates)):
+        print gate
         gate_x = gates[gate][1]
         gate_y = gates[gate][2]
         grid[gate_y][gate_x] = gates[gate][0]
--- print2.py
+++ print2.py
@@ -11,8 +11,11 @@ width = 17
 # Gate coordinates, template: gate_x, gate_y
 import csv
 gates = []
-with open('Print2.csv', 'rb') as csvfile:
+with open('print2.csv', 'rb') as csvfile:
     # Without QUOTE_NONNUMERIC the numbers become strings
     csvfile = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
     for row in csvfile:
         gates.append(row)
+
+
+print gates
