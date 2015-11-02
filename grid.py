f47128a7d7a07dda9d8981bf9baffc1cd763d2d9
mdhk <mdhk@users.noreply.github.com>
2015-11-02 14:30:47 +0100

    class Gate toegevoegd, gates zijn objecten

--- grid.py
+++ grid.py
@@ -1,16 +1,21 @@
 from config1 import width, height, gates
-
 depth = 1
+
+class Gate:
+    def __init__(self, gate_name, gate_x, gate_y):
+        self.name = gate_name
+        self.x = gate_x
+        self.y = gate_y
+    def __repr__(self):
+        return "%s" % self.name
 
 grid = [[[0 for z in range(depth)] for x in range(width)] for y in range(height)]
 
 def set_gates(gates):
     for gate in gates:
-         grid[gate[1]][gate[0]] = 1
-
+         grid[gate[1]][gate[0]] = Gate((gates.index(gate) + 1), gate[0], gate[1])
 
 set_gates(gates)
 
 for x in range(len(grid)):
     print grid[x]
-
