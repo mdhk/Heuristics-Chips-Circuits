6a4c01e037b1b5418f1e37960e3b0148419a48bf
Maurits <maurits.ridder@gmail.com>
2015-11-06 13:17:33 +0100

    working visualization

--- grid.py
+++ grid.py
@@ -1,9 +1,11 @@
 from data.config1 import width, height, gates
+import visualizations.array_backed_grid as draw
+
 depth = 1
 
 class Gate:
     def __init__(self, gate_name, gate_x, gate_y):
-        self.name = gate_name
+        self.name = "gate: " + str(gate_name)
         self.x = gate_x
         self.y = gate_y
     def __repr__(self):
@@ -11,11 +13,16 @@ class Gate:
 
 grid = [[[0 for z in range(depth)] for x in range(width)] for y in range(height)]
 
-def set_gates(gates):
+def set_gates(gates, grid):
     for gate in gates:
         grid[gate[1]][gate[0]][0] = Gate((gates.index(gate) + 1), gate[0], gate[1])
 
-set_gates(gates)
+set_gates(gates, grid)
 
 for x in range(len(grid)):
     print grid[x]
+
+screen = draw.initGrid()
+draw.drawGrid(grid, screen)
+
+import IPython; IPython.embed()
