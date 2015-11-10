2910c20e1eb96ba03161a3a62b57d1347abd9d47
Maurits <maurits.ridder@gmail.com>
2015-11-10 14:45:05 +0100

    Dijkstra

--- grid.py
+++ grid.py
@@ -4,23 +4,25 @@ import visualizations.array_backed_grid as draw
 depth = 8
 
 class Vertex:
-    def __init__(self, gate_name='', gate_x='', gate_y=''):
-        self.name = str(gate_name)
-        self.x = gate_x
-        self.y = gate_y
+    def __init__(self, vertex_label='', gate_x='', gate_y=''):
+        self.name = str(vertex_label)
     def __repr__(self):
         return "%s" % self.name
+
+# class PriorityQueue:
+#     def __init__(self):
+#         self.queue = []
 
 grid = [[[Vertex() for z in range(depth)] for x in range(width)] for y in range(height)]
 
 def set_gates(gates, grid):
     for gate in gates:
-        grid[gate[1]][gate[0]][0] = Vertex((gates.index(gate) + 1), gate[0], gate[1])
+        grid[gate[1]][gate[0]][0] = Vertex((gates.index(gate) + 1))
 
 set_gates(gates, grid)
 
-for x in range(len(grid)):
-    print grid[x]
+# for x in range(len(grid)):
+#     print grid[x]
 
 screen = draw.initGrid(width,height)
 draw.drawGrid(grid, screen, depth)
