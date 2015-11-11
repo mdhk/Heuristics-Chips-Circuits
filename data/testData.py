03836bb4972b15ba27091367911903eab0a3a291
Hasine Efeturk <hasineefeturk@hotmail.com>
2015-11-11 11:25:33 +0100

    .

--- data/testData.py
+++ data/testData.py
@@ -1,6 +1,7 @@
 # testData
 # Heuristics, Chips & Circuits, Fall 2015
 # HaMaMa
+import numpy 
 
 # grid width and height
 width = 5
@@ -8,8 +9,11 @@ height = 6
 
 # gate coordinates, template: [gate_number, gate_x, gate_y]
 gates = [
+		[1, 1],
+		[3, 2],
         [1, 3],
-        [1, 1],
         [2, 4],
-        [3, 2],
         ]
+
+# gate 0 = [1,1] .. gate 3= [2,4], Tussen de volgende gates loopt er een pad:
+netlist = [(0,1),(0,2),(2,3),(3,1)]
