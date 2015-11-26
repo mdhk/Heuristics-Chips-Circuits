61dae0334514f00ab786a0cf4ea9f33cf6e4a7c8
Maurits <maurits.ridder@gmail.com>
2015-11-26 18:15:31 +0100

    Core: separate applyPath functions into individual functions. runAlgorithm: edited to work with the new functions

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ WIDTH, HEIGHT etc. without hard-coding this into each module.
 
 from data.config1 import width as WIDTH, height as HEIGHT, gates
 from data.netlist import netlist_1 as netlist
-from algorithms import weird_aStar as algorithm
+from algorithms import aStar as algorithm
 
 DEPTH = 8
 SURF = WIDTH * HEIGHT
