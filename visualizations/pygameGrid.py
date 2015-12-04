93d5a2ac61a12f2b761e304af2840b571b24d85c
Maurits <maurits.ridder@gmail.com>
2015-12-04 15:51:15 +0100

    Add oneline visualization

--- visualizations/pygameGrid.py
+++ visualizations/pygameGrid.py
@@ -67,6 +67,46 @@ def drawGrid(grid, screen, depth):
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
+def allVisualization(g, gates, TOFIND):
+    import random
+
+    # Initialize visualization.
+    screen = initGrid(g.WIDTH, g.HEIGHT)
+    grid = [[[0 for c in range(g.DEPTH)] for b in range(g.WIDTH)] for a in
+            range(g.HEIGHT)]
+    # Color all gates bright red.
+    for i in gates:
+        grid[i[1]][i[0]][0] = (255, 0, 0)
+    depth = 0 
+
+    for p in range(TOFIND):
+        randcolor = (random.randint(0, 254), random.randint(0, 255), random.randint(0, 255))
+        for v in g:
+            if v.path == p and not v.gate:
+                grid[v.y][v.x][v.z] = randcolor
+                # Onhandig dat dit elke keer moet worden geroepen, maar dat komt
+                # omdat de kleur per call elke keer 1x random wordt beslist. 
+                drawGrid(grid, screen, depth)
+
+    # import IPython; IPython.embed()
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Visualize the layer up or down from current (left and right click
+            # respectively).
+            if event.button == 1 and depth != 7:
+                screen.fill([255,255,255])
+                depth += 1
+                drawGrid(grid, screen, depth)
+                print 'Showing layer: ' + str(depth)
+            if event.button == 3 and depth != 0:
+                screen.fill([255,255,255])
+                depth -= 1
+                drawGrid(grid, screen, depth)
+                print 'Showing layer: ' + str(depth)
+
  
 # Be IDLE friendly. If you forget this line, the program will 'hang'
 # on exit.
